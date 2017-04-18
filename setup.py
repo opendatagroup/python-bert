9b34beef68a90f80701be0619a75d3ecb4a48905
krant <aleksey.vasilenko@gmail.com>
2017-04-18 11:17:02 +0300

    Add pandas as dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email = 'samuel@descolada.com',
     url = 'https://github.com/samuel/python-bert',
     packages = ['bert'],
-    install_requires = ["basho-erlastic"],
+    install_requires = ["basho-erlastic", "pandas"],
     classifiers = [
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
